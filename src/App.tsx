185e0a5208af602c629f2f3b19d034827fc21db1
ZHENSHUO ZHAO <vincentjill@hotmail.com>
2025-10-24 18:16:39 +1100

    Added Tailwind, Headless UI and Hero Icons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AdjustmentsVerticalIcon } from "@heroicons/react/24/solid";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
+function Example() {
+  return (
+    <Menu>
+      <MenuButton>My account</MenuButton>
+      <MenuItems anchor="bottom">
+        <MenuItem>
+          <a className="block data-focus:bg-blue-100" href="/settings">
+            Settings
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a className="block data-focus:bg-blue-100" href="/support">
+            Support
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a className="block data-focus:bg-blue-100" href="/license">
+            License
+          </a>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <AdjustmentsVerticalIcon className="fill-amber-700 size-8" />
+        <p className="text-amber-600">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Example />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
