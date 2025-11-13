de6be2b875f32d48162f7541320574eb4d25ea5f
ZHENSHUO ZHAO <vincentjill@hotmail.com>
2025-11-13 14:33:28 +1100

    Updated AnimatedCardLayout structure to use layout animation properly with layoutId.

--- src/animations/AnimatedCardLayout.tsx
+++ src/animations/AnimatedCardLayout.tsx
@@ -17,7 +17,7 @@ export default function AnimatedCardLayout() {
       <div className="w-1/3 grid grid-cols-2 gap-4">
         {cards.map((c, i) => (
           <div onClick={() => setCardId(i)} key={i}>
-            {cardId !== i ? (
+            {
               <>
                 <motion.div
                   layoutId={`div${i}`}
@@ -29,31 +29,35 @@ export default function AnimatedCardLayout() {
                   cupiditate numquam, officiis minus incidunt veritatis suscipit
                   asperiores a, possimus perspiciatis!
                 </motion.div>
+                {cardId === i && (
+                  <>
+                    <motion.div
+                      onTap={() => setCardId(-1)}
+                      className="fixed inset-0 bg-black/50 z-10"
+                      initial={{ backdropFilter: "blur(0px)" }}
+                      animate={{ backdropFilter: "blur(4px)" }}
+                      exit={{ backdropFilter: "blur(0px)" }}
+                      transition={{ duration: 0.5 }}
+                    />
+                    <motion.div
+                      onTap={() => setCardId(-1)}
+                      layoutId={`div${i}`}
+                      className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-1/4 ${c.bgColor} text-lg rounded-xl p-6 ${c.hoverColor} z-20`}
+                      transition={{
+                        type: "spring",
+                        stiffness: 600,
+                        damping: 25,
+                      }}
+                    >
+                      {i}. Lorem, ipsum dolor sit amet consectetur adipisicing
+                      elit. Sunt ad omnis libero corrupti harum illo est dicta
+                      nobis, sed cupiditate numquam, officiis minus incidunt
+                      veritatis suscipit asperiores a, possimus perspiciatis!
+                    </motion.div>
+                  </>
+                )}
               </>
-            ) : (
-              <>
-                <div />
-                <motion.div
-                  onTap={() => setCardId(-1)}
-                  className="fixed inset-0 bg-black/50 z-10"
-                  initial={{ backdropFilter: "blur(0px)" }}
-                  animate={{ backdropFilter: "blur(4px)" }}
-                  exit={{ backdropFilter: "blur(0px)" }}
-                  transition={{ duration: 0.5 }}
-                />
-                <motion.div
-                  onTap={() => setCardId(-1)}
-                  layoutId={`div${i}`}
-                  className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-1/4 ${c.bgColor} text-lg rounded-xl p-6 ${c.hoverColor} z-20`}
-                  transition={{ type: "spring", stiffness: 600, damping: 25 }}
-                >
-                  {i}. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Sunt ad omnis libero corrupti harum illo est dicta nobis, sed
-                  cupiditate numquam, officiis minus incidunt veritatis suscipit
-                  asperiores a, possimus perspiciatis!
-                </motion.div>
-              </>
-            )}
+            }
           </div>
         ))}
       </div>
--- src/animations/AnimatedTabs.tsx
+++ src/animations/AnimatedTabs.tsx
@@ -35,23 +35,31 @@ export default function AnimatedTabs() {
 
   return (
     <MotionContainer>
-      <div className="w-1/3 flex justify-between items-start">
-        {origins.map((o) => (
-          <div onClick={() => setOrigin(o)} className="flex-1">
-            <div
-              data-selected={selectedOrigin === o ? true : null}
-              className={`text-center text-lg font-semibold rounded-tl-lg rounded-tr-lg text-slate-400 hover:bg-slate-700 ${o.textColor} transition duration-500`}
-            >
-              {o.text}
+      <div className="flex flex-col items-center gap-2">
+        <div className="w-1/3 flex justify-between items-start">
+          {origins.map((o) => (
+            <div onClick={() => setOrigin(o)} className="flex-1">
+              <div
+                data-selected={selectedOrigin === o ? true : null}
+                className={`text-center text-lg font-semibold rounded-tl-lg rounded-tr-lg text-slate-400 hover:bg-slate-700 ${o.textColor} transition duration-500`}
+              >
+                {o.text}
+              </div>
+              {selectedOrigin === o && (
+                <motion.div
+                  layoutId="underline"
+                  className={`h-1 w-full ${o.bgColor}`}
+                />
+              )}
             </div>
-            {selectedOrigin === o && (
-              <motion.div
-                layoutId="underline"
-                className={`h-1 w-full ${o.bgColor}`}
-              />
-            )}
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="w-1/3">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam cum
+          saepe ducimus cumque dolor, doloremque, quo exercitationem atque
+          molestiae quidem, dolores mollitia fugit aliquam blanditiis aut modi!
+          Impedit, voluptatem ratione!
+        </div>
       </div>
     </MotionContainer>
   );
