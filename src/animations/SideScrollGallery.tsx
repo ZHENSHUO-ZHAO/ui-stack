3deec7f8d18c2d742cffd6675f6fae493d54801b
ZHENSHUO ZHAO <vincentjill@hotmail.com>
2025-11-15 00:15:25 +1100

    Added scroll progress of child items inside a scroll container.

--- src/animations/SideScrollGallery.tsx
+++ src/animations/SideScrollGallery.tsx
@@ -19,32 +19,66 @@ export default function SideScrollGallery() {
     <MotionContainer>
       <motion.div
         ref={scrollRef}
-        className="w-1/3 h-1/4 overflow-x-auto snap-x snap-proximity"
+        className="relative w-1/3 h-1/4 overflow-x-auto snap-x snap-proximity flex justify-start items-stretch gap-2"
         style={{ maskImage }}
       >
-        <div className="flex justify-start items-stretch gap-2">
+        <>
           {[1, 2, 3, 4, 5, 6, 7].map((i) => (
-            <div className="flex-[1_0_300px] p-4 text-lg font-semibold bg-teal-700 rounded-2xl snap-center">
-              [{i}]. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Distinctio, veniam facilis similique eveniet cum vel, natus hic
-              odio blanditiis accusantium expedita architecto nesciunt officiis
-              doloremque dolor qui vitae fuga totam.
-            </div>
+            <ItemIndicator key={i} i={i} scrollRef={scrollRef} />
           ))}
-        </div>
+        </>
       </motion.div>
     </MotionContainer>
   );
 }
 
-const left = `0%`;
-const leftInset = `10%`;
-const rightInset = `90%`;
-const right = `100%`;
-const transparent = `#0000`;
-const opaque = `#000`;
+function ItemIndicator({
+  i,
+  scrollRef,
+}: {
+  i: number;
+  scrollRef: React.RefObject<HTMLDivElement | null>;
+}) {
+  const targetRef = useRef<HTMLDivElement>(null);
+  const { scrollXProgress } = useScroll({
+    container: scrollRef,
+    target: targetRef,
+    axis: "x",
+    offset: ["end start", "start end"],
+  });
+
+  return (
+    <div
+      ref={targetRef}
+      className="flex-none min-w-[300px] max-w-[350px] p-4 text-lg font-semibold bg-teal-700 rounded-2xl snap-center"
+    >
+      <svg className="mr-2 size-[38px] stroke-8 stroke-amber-300 fill-transparent float-left translate-y-2">
+        <motion.circle
+          cx="50%"
+          cy="50%"
+          r="15"
+          strokeLinecap="round"
+          pathLength={scrollXProgress}
+        />
+      </svg>
+      <p>
+        [{i}]. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Distinctio, veniam facilis similique eveniet cum vel, natus hic odio
+        blanditiis accusantium expedita architecto nesciunt officiis doloremque
+        dolor qui vitae fuga totam.
+      </p>
+    </div>
+  );
+}
 
 function useImageMask(scrollXProgress: MotionValue<number>) {
+  const left = `0%`;
+  const leftInset = `10%`;
+  const rightInset = `90%`;
+  const right = `100%`;
+  const transparent = `#0000`;
+  const opaque = `#000`;
+
   const maskImage = useMotionValue(
     `linear-gradient(90deg, ${opaque}, ${opaque} ${left}, ${opaque} ${rightInset}, ${transparent})`
   );
